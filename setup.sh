af9705f766366fd6e02f3affb33126bbf859a06e
djtoler <84260551+djtoler@users.noreply.github.com>
2022-10-05 17:40:01 -0400

    update server packages

---
+++ setup.sh
@@ -0,0 +1,42 @@
+echo "Project name?"
+read Project
+echo "Project created: $Project"
+echo "React or HBS? (Type r or h)"
+read Client
+echo "Client is $Client"
+mkdir "$Project"
+cd "$Project"
+mkdir server
+mkdir client
+touch server/index.js
+mkdir server/functions
+mkdir server/database
+#conditional
+if [ "$Client" != "r" ]
+then
+mkdir src/views
+mkdir src/public
+fi
+#end conditional
+touch .gitignore
+echo "node_modules/" >> .gitignore
+npm init -y
+sed '/"test":/d' package.json >temp.json
+sed '/"scripts": {/ a  "start": "ts-node src/index.ts",\n  "start:dev": "nodemon src/index.ts",\n  "test": "jest",\n  "test:dev": "jest --watchAll --verbose"' temp.json >temp2.json
+cat temp2.json >package.json
+rm temp2.json temp.json
+#conditional
+if [ "$Client" != "r" ]
+then
+npm i express cors hbs
+else
+npm i express cors aws-sdk redis chakra-ui/react axios react-router-dom react-dom uuid cryptr
+fi
+#end conditional
+
+#conditional
+if [ "$Client" = "r" ]
+then
+npx create-react-app client --template typescript
+echo "created client folder"
+fi
